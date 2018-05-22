554a6a970de1bfe42ad0d39d7be792d5b44a4155
zxfjd3g <258147149@qq.com>
2018-05-22 09:32:20 +0800

    总结

--- README.md
+++ README.md
@@ -143,25 +143,45 @@
     
 # day04
 ## 1. ShopGoods组件
+    1). 动态展现列表数据
+    2). 基本滑动:
+        使用better-scroll
+        理解其基本原理
+        创建BScroll对象的时机
+          watch + $nextTick()
+          callback + $nextTick
+    3). 滑动右侧列表, 左侧同步更新
+        better-scroll禁用了原生的dom事件, 使用的是自定义事件
+        绑定监听: scroll/scrollEnd
+        滚动监听的类型: probeType
+        列表滑动的3种类型
+            手指触摸
+            惯性
+            编码
+        分析:
+            类名: current 标识当前分类
+            设计一个计算属性: currentIndex
+            根据哪些数据计算?
+              scrollY: 右侧滑动的Y轴坐标 (滑动过程时实时变化)
+              tops: 所有右侧分类li的top组成的数组  (列表第一次显示后就不再变化)
+        编码:
+            1. 在滑动过程中, 实时收集scrollY
+            2. 列表第一次显示后, 收集tops
+            3. 实现currentIndex的计算逻辑
+    4). 点击左侧列表项, 右侧滑动到对应位置
+    
 ## 2. CartControl组件
+    1). 问题: 更新状态数据, 对应的界面不变化
+        原因: 一般方法给一个已有绑定的对象中添加一个新的属性, 这个属性没有数据绑定
+        解决: 
+            Vue.set(obj, 'xxx', value)才有数据绑定
+            this.$set(obj, 'xxx', value)才有数据绑定
+            
 ## 3. ShopCart组件
+    1). 使用vuex管理购物项数据: cartFoods
+    2). 解决几个功能性bug
+
 ## 4. Food组件
-
-### 功能: 
-    实现2个列表滑动
-    当前分类
-    当滑动右侧列表时, 更新当前分类
-    点击某个分类项, 右侧列表滑动到对应位置
-
-
-### 分析:
-    类名: current 标识当前分类
-    设计一个计算属性: currentIndex
-    根据哪些数据计算?
-      scrollY: 右侧滑动的Y轴坐标 (滑动过程时实时变化)
-      tops: 所有右侧分类li的top组成的数组  (列表第一次显示后就不再变化)
-
-### 编码:
-    1. 在滑动过程中, 实时收集scrollY
-    2. 列表第一次显示后, 收集tops
-    3. 实现currentIndex的计算逻辑
+    1). 父子组件:
+        子组件调用父组件的方法: 通过props将方法传递给子组件
+        父组件调用子组件的方法: 通过ref找到子组件标签对象
--- src/components/ShopCart/ShopCart.vue
+++ src/components/ShopCart/ShopCart.vue
@@ -18,23 +18,26 @@
           </div>
         </div>
       </div>
-      <div class="shopcart-list" v-show="listShow">
-        <div class="list-header">
-          <h1 class="title">购物车</h1>
-          <span class="empty">清空</span>
+      <transition name="move">
+        <div class="shopcart-list" v-show="listShow">
+          <div class="list-header">
+            <h1 class="title">购物车</h1>
+            <span class="empty">清空</span>
+          </div>
+          <div class="list-content">
+            <ul>
+              <li class="food" v-for="(food, index) in cartFoods" :key="index">
+                <span class="name">{{food.name}}</span>
+                <div class="price"><span>￥{{food.price}}</span></div>
+                <div class="cartcontrol-wrapper">
+                  <CartControl :food="food"/>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div class="list-content">
-          <ul>
-            <li class="food" v-for="(food, index) in cartFoods" :key="index">
-              <span class="name">{{food.name}}</span>
-              <div class="price"><span>￥{{food.price}}</span></div>
-              <div class="cartcontrol-wrapper">
-                <CartControl :food="food"/>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
+      </transition>
+
     </div>
     <div class="list-mask" v-show="listShow" @click="toggleShow"></div>
   </div>
@@ -209,9 +212,9 @@
       z-index -1
       width 100%
       transform translateY(-100%)
-      &.swipe-enter-active, &.swipe-leave-active
+      &.move-enter-active, &.move-leave-active
         transition transform .3s
-      &.swipe-enter, &.swipe-leave-to
+      &.move-enter, &.move-leave-to
         transform translateY(0)
       .list-header
         height 40px
