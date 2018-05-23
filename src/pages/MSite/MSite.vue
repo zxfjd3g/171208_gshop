70e3c41592e7603818459ca212c53e87ce0fb3a5
zxfjd3g <258147149@qq.com>
2018-05-23 19:07:21 +0800

    总结

--- src/pages/MSite/MSite.vue
+++ src/pages/MSite/MSite.vue
@@ -2,7 +2,6 @@
   <section class="msite">
     <!--首页头部-->
     <HeaderTop :title="address.name">
-
       <router-link class="header_search" slot="left" to="/search">
         <i class="iconfont icon-sousuo"></i>
       </router-link>
@@ -15,37 +14,42 @@
         </span>
       </router-link>
     </HeaderTop>
-    <!--首页导航-->
-    <nav class="msite_nav">
-      <div class="swiper-container" v-if="categorys.length">
-        <div class="swiper-wrapper">
-          <div class="swiper-slide" v-for="(categorys, index) in categorysArr" :key="index">
-            <a href="javascript:" class="link_to_food" v-for="(category, index) in categorys" :key="index">
-              <div class="food_container">
-                <img :src="baseImageUrl+category.image_url">
+    <div class="miste-content-wrapper">
+      <div class="miste-content">
+        <!--首页导航-->
+        <nav class="msite_nav">
+          <div class="swiper-container" v-if="categorys.length">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide" v-for="(categorys, index) in categorysArr" :key="index">
+                <a href="javascript:" class="link_to_food" v-for="(category, index) in categorys" :key="index">
+                  <div class="food_container">
+                    <img :src="baseImageUrl+category.image_url">
+                  </div>
+                  <span>{{category.title}}</span>
+                </a>
               </div>
-              <span>{{category.title}}</span>
-            </a>
-          </div>
 
+            </div>
+            <!-- Add Pagination -->
+            <div class="swiper-pagination"></div>
+          </div>
+          <img src="./images/msite_back.svg" alt="back" v-else>
+        </nav>
+        <!--首页附近商家-->
+        <div class="msite_shop_list">
+          <div class="shop_header">
+            <i class="iconfont icon-xuanxiang"></i>
+            <span class="shop_header_title">附近商家</span>
+          </div>
+          <ShopList/>
         </div>
-        <!-- Add Pagination -->
-        <div class="swiper-pagination"></div>
       </div>
-      <img src="./images/msite_back.svg" alt="back" v-else>
-    </nav>
-    <!--首页附近商家-->
-    <div class="msite_shop_list">
-      <div class="shop_header">
-        <i class="iconfont icon-xuanxiang"></i>
-        <span class="shop_header_title">附近商家</span>
-      </div>
-      <ShopList/>
     </div>
   </section>
 </template>
 
 <script>
+  import BScroll from 'better-scroll'
   import {mapState} from 'vuex'
   import Swiper from 'swiper'
   import 'swiper/dist/css/swiper.min.css'
@@ -122,10 +126,15 @@
               el: '.swiper-pagination',
             },
           })
+
+          new BScroll('.miste-content-wrapper', {
+            click: true
+          })
         })
 
       }
     },
+
     components: {
       HeaderTop,
       ShopList
@@ -137,54 +146,59 @@
   @import "../../common/stylus/mixins.styl"
   .msite  //首页
     width 100%
-    .msite_nav
-      bottom-border-1px(#e4e4e4)
-      margin-top 45px
-      height 200px
-      background #fff
-      .swiper-container
-        width 100%
-        height 100%
-        .swiper-wrapper
+    .miste-content-wrapper
+      position fixed
+      top: 45px
+      bottom: 46px
+      width: 100%
+      .msite_nav
+        bottom-border-1px(#e4e4e4)
+        margin-top 15px
+        height 200px
+        background #fff
+        .swiper-container
           width 100%
           height 100%
-          .swiper-slide
-            display flex
-            justify-content center
-            align-items flex-start
-            flex-wrap wrap
-            .link_to_food
-              width 25%
-              .food_container
-                display block
-                width 100%
-                text-align center
-                padding-bottom 10px
-                font-size 0
-                img
-                  display inline-block
-                  width 50px
-                  height 50px
-              span
-                display block
-                width 100%
-                text-align center
-                font-size 13px
-                color #666
-        .swiper-pagination
-          >span.swiper-pagination-bullet-active
-            background #02a774
-    .msite_shop_list
-      top-border-1px(#e4e4e4)
-      margin-top 10px
-      background #fff
-      .shop_header
-        padding 10px 10px 0
-        .shop_icon
-          margin-left 5px
-          color #999
-        .shop_header_title
-          color #999
-          font-size 14px
-          line-height 20px
+          .swiper-wrapper
+            width 100%
+            height 100%
+            .swiper-slide
+              display flex
+              justify-content center
+              align-items flex-start
+              flex-wrap wrap
+              .link_to_food
+                width 25%
+                .food_container
+                  display block
+                  width 100%
+                  text-align center
+                  padding-bottom 10px
+                  font-size 0
+                  img
+                    display inline-block
+                    width 50px
+                    height 50px
+                span
+                  display block
+                  width 100%
+                  text-align center
+                  font-size 13px
+                  color #666
+          .swiper-pagination
+            >span.swiper-pagination-bullet-active
+              background #02a774
+      .msite_shop_list
+        top-border-1px(#e4e4e4)
+        margin-top 10px
+        background #fff
+        .shop_header
+          padding 10px 10px 0
+          .shop_icon
+            margin-left 5px
+            color #999
+          .shop_header_title
+            color #999
+            font-size 14px
+            line-height 20px
 </style>
